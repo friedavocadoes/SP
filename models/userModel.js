6a9bc1d16972c94e53baa63f1bd3d214296e1042
gautham <desertncamel@gmail.com>
2024-09-26 15:48:04 +0530

    usermodel change

--- models/userModel.js
+++ models/userModel.js
@@ -1,11 +1,6 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        required: [true, "Please provide username"],
-        unique: true,
-    },
     email: {
         type: String,
         required: [true, "Please provide email"],
